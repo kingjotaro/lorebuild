cd07835917ffcd15c78cf5edeef69cf27052244e
kingjotaro <rafaelleet@gmail.com>
2023-07-17 07:33:25 -0300

    add auto play music login page

--- components/login.js
+++ components/login.js
@@ -1,5 +1,5 @@
-import Image from "next/image";
-import { useState } from "react";
+import React, { useRef } from 'react';
+import { useState, useEffect } from "react";
 import Spline from "@splinetool/react-spline";
 import LoginForm from "./loginform";
 
@@ -10,29 +10,38 @@ export default function Login() {
     setIsImageLoaded(true);
   }
 
+  const audioRef = useRef(null);
+
+ useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.play()}
+  }, []);
+
   return (
-    <div >
-
-<div className="fixed inset-0 z-0">
-      <Spline className="hidden sm:block"
-        scene="https://prod.spline.design/88Yayv49dX4j4FSG/scene.splinecode"
-        onLoad={splineLoad}
-      />
-       
-      {/* The container takes up the full width and height of the viewport. */}
-      <img
-        src="/mobilebg.png"
-        alt="img estatica"
-        className="block sm:hidden w-screen h-screen"
-      />
-
-        
-     
-
-      {isImageLoaded && (<LoginForm> </LoginForm>)
-        
-        }
+    <div>
+      <div className="fixed inset-0 z-0">
+      <div>
+      <audio ref={audioRef} controls autoPlay >
+  <source src="village.mp3" type="audio/mpeg" />
+  Seu navegador não suporta a reprodução de áudio.
+</audio>
     </div>
+
+        <Spline
+          className="hidden sm:block"
+          scene="https://prod.spline.design/88Yayv49dX4j4FSG/scene.splinecode"
+          onLoad={splineLoad}
+        />
+
+        {/* The container takes up the full width and height of the viewport. */}
+        <img
+          src="/mobilebg.png"
+          alt="img estatica"
+          className="block sm:hidden w-screen h-screen"
+        />
+
+        {isImageLoaded && <LoginForm> </LoginForm>}
+      </div>
     </div>
   );
 }
